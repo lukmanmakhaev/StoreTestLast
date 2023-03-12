4b29a1d4d32cdaa5b9ec598ca0b82e127d86e3fb
Lukman <lukmanmakhaev@gmail.com>
2023-03-12 15:50:37 +0300

    Initial Commit

---
+++ StoreTest/StoreTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreTestApp.swift
+//  StoreTest
+//
+//  Created by Lukman Makhaev on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct StoreTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
